770a5bf12cabd3a3b2137627b53d1c5ff9e30e7b
Aom Siriwat <siriwat576@gmail.com>
2025-06-12 14:23:07 +0700

    chore: Add `sessionId` to `KeyIdentityRegistry`

--- src/constants/registries/KeyIdentity.ts
+++ src/constants/registries/KeyIdentity.ts
@@ -1,12 +1,14 @@
 import { RegistryItemHelper } from "@libs/ur-helper/registry";
 
 type Payload = {
+    readonly sessionId: string;
     readonly fingerprint: string;
     readonly publicKey: string;
 };
 export const KeyIdentityRegistry = RegistryItemHelper.createKeyMap<Record<keyof Payload, number>, Payload>({
-    fingerprint: 1,
-    publicKey: 2,
+    sessionId: 1,
+    fingerprint: 2,
+    publicKey: 3,
 }, "identity");
 export const createKeyIdentity = (payload: Payload) => new KeyIdentityRegistry(payload);
 export type KeyIdentityRegistryItem = ReturnType<typeof createKeyIdentity>;
