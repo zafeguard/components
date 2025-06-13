7c21445e4267319c0b7ddfc87a9d2b10573f3e50
Aom Siriwat <siriwat576@gmail.com>
2025-06-13 14:44:15 +0700

    chore: Add nested object for Partial Signature registry

--- src/constants/registries/PartialSignature.ts
+++ src/constants/registries/PartialSignature.ts
@@ -1,12 +1,24 @@
 import { RegistryItemHelper } from "@libs/ur-helper/registry";
 
+type ValuePayload = {
+    readonly messageHash: string;
+    readonly partialSignature: string;
+};
 type Payload = {
-    readonly values: string[];
+    readonly values: ValuePayload[];
     readonly publicKey: string;
 };
+
+export const PartialSignatureItemRegistry = RegistryItemHelper.createKeyMap<Record<keyof ValuePayload, number>, ValuePayload>({
+    messageHash: 1,
+    partialSignature: 2,
+}, "partial-signature-item");
+
 export const PartialSignatureRegistry = RegistryItemHelper.createKeyMap<Record<keyof Payload, number>, Payload>({
     values: 1,
     publicKey: 2,
 }, "partial-signature");
-export const createPartialSignature = (payload: Payload) => new PartialSignatureRegistry(payload);
+export const createPartialSignature = (payload: Payload) => new PartialSignatureRegistry(Object.assign(payload, {
+    values: payload.values.map(value => new PartialSignatureItemRegistry(value))
+}));
 export type PartialSignatureRegistryItem = ReturnType<typeof createPartialSignature>;
